1c8e3b25126704e6f076f995f7e5c9bcd97a1a93
Xuyang Kang <xuyangkang@gmail.com>
2013-08-24 02:21:57 +0800

    code refined

--- ANN.r
+++ ANN.r
@@ -1,71 +1,74 @@
-layer_size <- 4
-neuron_size <- c(28 * 28, 200, 200, 10) + 1
+#!/usr/bin/Rscript
+layer.size <- 4
+neuron.size <- c(28 * 28, 200, 200, 10) + 1
 synapse <- list()
-for (i in 2:layer_size) {
-  rand_data <- rnorm(n = neuron_size[i - 1] * neuron_size[i], mean = 0, sd = 0.1)
-  synapse <- c(synapse, list(matrix(data = rand_data, nrow = neuron_size[i - 1], ncol = neuron_size[i])))
+for (i in 2:layer.size) {
+  rand.data <- rnorm(n = neuron.size[i - 1] * neuron.size[i], mean = 0, sd = 0.1)
+  synapse <- c(synapse, list(matrix(data = rand.data, nrow = neuron.size[i - 1], ncol = neuron.size[i])))
 }
 
-pf_in <- file("train-images", "rb")
-pf_out <- file("train-labels", "rb")
+pf.in <- file("train-images", "rb")
+pf.out <- file("train-labels", "rb")
 
 #check magic
-magic <-readBin(pf_in, integer(), 1, endian = "big")
+magic <-readBin(pf.in, integer(), 1, endian = "big")
 stopifnot(magic == 2051)
-magic <-readBin(pf_out, integer(), 1, endian = "big")
+magic <-readBin(pf.out, integer(), 1, endian = "big")
 stopifnot(magic == 2049)
 
 #read and check train data numbers
-n <- readBin(pf_in, integer(), 1, endian = "big")[1]
-stopifnot(n == readBin(pf_out, integer(), 1, endian = "big"))
+n <- readBin(pf.in, integer(), 1, endian = "big")[1]
+stopifnot(n == readBin(pf.out, integer(), 1, endian = "big"))
 
 # read number of rows and columns
-m <- readBin(pf_in, integer(), 1, endian = "big")[1] * readBin(pf_in, integer(), 1, endian = "big")[1]
+m <- readBin(pf.in, integer(), 1, endian = "big")[1] * readBin(pf.in, integer(), 1, endian = "big")[1]
 
 # average error for all train data
 avg <- 0
 
 for (ii in 1:n) {
-
   # learning rate, a magic number...
-  learning_rate <- 0.009 * (n - ii) / n / 3 + 0.002
-  vec_in <- readBin(pf_in, integer(), n = m, size = 1)
-  vec_in <- (vec_in + 256) %% 256 / 256.0
-  vec_in <- c(vec_in, 1)
-  ac <- list(t(as.matrix(vec_in)))
-  for (i in 2:layer_size) {
-    ac_next <- ac[[i - 1]] %*% synapse[[i - 1]]
+  learning.rate <- 0.009 * (n - ii) / n / 3 + 0.002
+  vec.in <- readBin(pf.in, integer(), n = m, size = 1)
+  vec.in <- (vec.in + 256) %% 256 / 256.0
+  vec.in <- c(vec.in, 1)
+  ac <- list(t(as.matrix(vec.in)))
+  for (i in 2:layer.size) {
+    ac.next <- ac[[i - 1]] %*% synapse[[i - 1]]
     #active function : x > 0 ? x : 0
     rectifier <- function(x) { (abs(x) + x) * 0.5}
-    ac_next <- rectifier(ac_next)
-    ac_next[neuron_size[i]] <- 1
-    ac <- c(ac, list(ac_next))
+    ac.next <- rectifier(ac.next)
+    ac.next[neuron.size[i]] <- 1
+    ac <- c(ac, list(ac.next))
   }
-  
-  result <- ac[[layer_size]][1:10]
-  result <- exp(result)
+
+  #do softmax for output layer
+  result <- exp(ac[[layer.size]][1:10])
   normalizer <- function(x) { x / sum(x) }
   result <- normalizer(result)
-  label <- readBin(pf_out, integer(), size = 1) + 1
+
+  label <- readBin(pf.out, integer(), size = 1) + 1
   error <- rep(0.0, 10)
   error[label] <- 1.0
+
+  # I don't know why.... My GF tell me to do so and it works....
+  # Maybe I should learn some more ML...
   result <- result - error
   avg <- (avg * (ii - 1) + sum(error * result)) / ii
-  error <- result
-  if (ii %% 10 == 0) print(avg)
+
+  if (ii %% 100 == 0) print(avg)
   if (is.nan(avg)) break
-  error <- as.matrix(c(error, 0))    
+  error <- as.matrix(c(result, 0))    
   
-  for (i in layer_size:2) {
-    delta <- t(error %*% ac[[i - 1]])
-    delta <- delta + synapse[[i - 1]] * 0.0001
-    new_error <- t(synapse[[i - 1]] %*% error) 
-    synapse[[i - 1]] <- synapse[[i - 1]] - delta * learning_rate
-    for (j in 1:ncol(new_error)) {
-      if (ac[[i - 1]][j] == 0) new_error[j] <- 0
+  for (i in layer.size:2) {
+    delta <- t(error %*% ac[[i - 1]]) + synapse[[i - 1]] * 0.0001
+    new.error <- t(synapse[[i - 1]] %*% error) 
+    synapse[[i - 1]] <- synapse[[i - 1]] - delta * learning.rate
+    for (j in 1:ncol(new.error)) {
+      if (ac[[i - 1]][j] == 0) new.error[j] <- 0
     }
-    error <- new_error
-    error[neuron_size[i]] <- 0
-    error <- t(error)
+    new.error[neuron.size[i]] <- 0
+    error <- t(new.error)
   }
 }
+save.image()
